ff06c42bb0b2f70983ae34da080f46e3f27fd4d3
Saurabh Kumar <saurabhkr9902@gmail.com>
2022-04-07 16:08:15 +0530

    Updates

---
+++ precache-manifest.0d7b1ea3d08ec1fda4e8c13b715edede.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bfaf27fba1931d42c8be3ad74045868",
+    "url": "/saurabhkr99/portfolio-website/index.html"
+  },
+  {
+    "revision": "c45218aaa05fe72a0962",
+    "url": "/saurabhkr99/portfolio-website/static/css/2.d88a60dc.chunk.css"
+  },
+  {
+    "revision": "8cdc519a9f2e737f40d2",
+    "url": "/saurabhkr99/portfolio-website/static/css/main.f1dfb855.chunk.css"
+  },
+  {
+    "revision": "c45218aaa05fe72a0962",
+    "url": "/saurabhkr99/portfolio-website/static/js/2.179a743a.chunk.js"
+  },
+  {
+    "revision": "676807117e2f4e847886ee878f1384eb",
+    "url": "/saurabhkr99/portfolio-website/static/js/2.179a743a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cdc519a9f2e737f40d2",
+    "url": "/saurabhkr99/portfolio-website/static/js/main.46f9c830.chunk.js"
+  },
+  {
+    "revision": "445ea38ec320ded0cc00",
+    "url": "/saurabhkr99/portfolio-website/static/js/runtime-main.90328946.js"
+  },
+  {
+    "revision": "07027ce172545a93cda52285194f0a0d",
+    "url": "/saurabhkr99/portfolio-website/static/media/person1.07027ce1.jpg"
+  }
+]);
